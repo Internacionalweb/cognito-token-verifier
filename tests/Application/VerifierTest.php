4c72294c4c9841bfed8aa4d60daf49fffa3611a5
Adrián Martín <contacto@adrianmr.com>
2023-03-06 20:21:49 +0100

    ✅ Added more tests on verifier and improved naming

--- tests/Application/VerifierTest.php
+++ tests/Application/VerifierTest.php
@@ -3,31 +3,52 @@
 namespace CognitoTokenVerifier\Tests\Application;
 
 use DG\BypassFinals;
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use CognitoTokenVerifier\Application\Verifier;
+use CognitoTokenVerifier\Domain\KeysRepository;
+use CognitoTokenVerifier\Domain\Exceptions\InvalidTokenDecodedException;
 
 final class VerifierTest extends TestCase
 {
+    private KeysRepository $keysRepositoryMock;
+    private Verifier $verifier;
+
     public function setUp(): void
     {
         BypassFinals::enable();
+
+        $this->keysRepositoryMock = $this->createMock(KeysRepository::class);
+        $this->verifier = new Verifier($this->keysRepositoryMock);
     }
 
     /** @test */
-    public function whenTokenIsMalformed(): void
+    public function it_should_throw_an_invalid_token_decoded_exception_when_the_token_is_malformed(): void
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(InvalidTokenDecodedException::class);
+        $this->expectExceptionMessage('The token have an incorrect format or is not valid.');
+        $this->expectExceptionCode(401);
 
-        /**
-         * @var \PHPUnit\Framework\MockObject\MockObject $mockAuthenticator
-         */
-        $mockAuthenticator = $this->createMock(Verifier::class);
-        $mockAuthenticator->method('__invoke')->willThrowException(new InvalidArgumentException());
+        $this->verifier->__invoke('token', []);
+        
+    }
 
-        /**
-         * @var Verifier $mockAuthenticator
-         */
-        $mockAuthenticator->__invoke('token', []);
+    /** @test */
+    public function it_should_throw_an_invalid_token_decoded_exception_when_the_token_is_not_a_string(): void
+    {
+        $this->expectException(InvalidTokenDecodedException::class);
+        $this->expectExceptionMessage('The token have an incorrect format or is not valid.');
+        $this->expectExceptionCode(401);
+
+        $this->verifier->__invoke(123, []);
+    }
+
+    /** @test */
+    public function it_should_throw_an_invalid_token_decoded_exception_when_the_token_is_empty(): void
+    {
+        $this->expectException(InvalidTokenDecodedException::class);
+        $this->expectExceptionMessage('The token have an incorrect format or is not valid.');
+        $this->expectExceptionCode(401);
+
+        $this->verifier->__invoke('', []);
     }
 }
