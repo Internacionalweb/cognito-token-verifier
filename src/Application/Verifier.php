9b6f47fc566403252120792647c7a17f8a966482
Adrián Martín <contacto@adrianmr.com>
2023-03-12 11:58:29 +0100

    📝 Added param documentation

--- src/Application/Verifier.php
+++ src/Application/Verifier.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CognitoTokenVerifier\Application;
 
-use InvalidArgumentException;
 use CognitoTokenVerifier\Domain\Token;
 use CognitoTokenVerifier\Domain\KeysRepository;
 use CognitoTokenVerifier\Infrastructure\BearerTokenParser;
@@ -21,6 +20,11 @@ final class Verifier
     private Token $token;
     private string $cognitoUserPoolRegion;
 
+    /**
+     * @param string                        $cognitoUserPoolId                  AWS Cognito User Pool Id (You can find it in the AWS Cognito User Pool console) (Example: us-east-1_XXXXXXXXX)
+     * @param KeysRepository                $keysRepository                     Keys Repository (You can use the one provided in the package 'FromFileKeysRepository' or create your own, just implement the KeysRepository interface)
+     * @param string|null                   $cognitoAppClientsIdsAllowed        AWS Cognito APPs Client Ids allowed (Separated by comma) (Optional if you dont want to validate Apps just keep it on null)
+     */
     public function __construct(
         private string $cognitoUserPoolId,
         private KeysRepository $keysRepository,
@@ -30,18 +34,17 @@ final class Verifier
     }
 
     /**
-     * @param  string                       $bearerToken
-     * @param  array<int,string>|null       $requiredResourceServerScopes
-     * @throws InvalidArgumentException
-     * @throws InvalidTokenException
-     * @throws InvalidSignature
-     * @throws InvalidTokenExpiredException
-     * @throws InvalidClientException
-     * @throws InvalidUserPoolException
-     * @throws InvalidUseException
-     * @throws InvalidScopesException
+     * @param  string                       $bearerToken                    Bearer Token from Cognito Auth
+     * @param  array<string>|null           $requiredScopes                 Scopes that the token must have to be valid (Optional if you dont want to validate scopes just keep it on null)
+     * @throws InvalidTokenException        In case the token dont have the required parameters (kid, iss, tokenUse, clientId, scope, exp)
+     * @throws InvalidSignature             In case the token signature is invalid (kid not found on JWK)
+     * @throws InvalidTokenExpiredException In case the token is expired
+     * @throws InvalidClientException       In case the token clientId is not on the list of allowed clients $cognitoAppClientsIdsAllowed (if you dont provide a list of allowed clients on constructor this exception will never be thrown)
+     * @throws InvalidUserPoolException     In case the token issuer dont match with the user pool id provided on constructor
+     * @throws InvalidUseException          In case the token use is not 'access'
+     * @throws InvalidScopesException       In case the token dont have the required scopes (if you dont provide a list of required scopes on $requiredScopes this exception will never be thrown)
      */
-    public function __invoke(string $bearerToken, ?array $requiredResourceServerScopes = null): void
+    public function __invoke(string $bearerToken, ?array $requiredScopes = null): void
     {
         $this->token = BearerTokenParser::decode($bearerToken);
 
@@ -55,8 +58,8 @@ final class Verifier
             $this->ensureTokenAppClientIdItsAllowed();
         }
 
-        if ($requiredResourceServerScopes) {
-            $this->ensureRequiredScopesExistsOnToken($requiredResourceServerScopes);
+        if ($requiredScopes) {
+            $this->ensureRequiredScopesExistsOnToken($requiredScopes);
         }
     }
 
